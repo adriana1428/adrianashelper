21eff1e14bc00425fed4e6a093f04c0595ce7029
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-03 17:57:40 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -4,6 +4,23 @@ const bot = new Discord.Client();
 bot.on("ready", () => {
     console.log("Estoy listo!");
  });
+
+ const prefix = "h!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);                     
+  }    
+});
+
  
  bot.on("message", (message) => {
    if(message.content.startsWith("h!hi")) {
