088674cfb6a36a919d2edb9a9e0549fb1f099a9b
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-03 18:37:15 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -18,7 +18,12 @@ bot.on("message", function(message) {
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);                     
-  }    
+  }
+  else if (command === "sum") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`The sum of all the arguments you provided is ${sum}!`);                          
+  }                            
 });
 
  
