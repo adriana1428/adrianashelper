a0296bc3d8c0a40b1a219038690e8bcadcb0f652
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-03 18:45:41 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -19,11 +19,11 @@ bot.on("message", function(message) {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);                     
   }
-  else if (command === "suma") {
+  else if (command === "sum") {
     const numArgs = args.map(x => parseFloat(x));
-    const suma = numArgs.reduce((counter, x) => counter += x);
-    message.reply(`El total de esta suma es ${suma}!`);                          
-  }                            
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`The sum of all the arguments you provided is ${sum}!`);
+  }                  
 });
 
  
