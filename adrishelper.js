2981596c8cf69b4c00c2fa9ffdf266b99c067bc6
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-03 11:57:40 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -10,25 +10,8 @@ bot.on("ready", function() {
      message.channel.send("Hola, soy la ayudante de Adriana");
    }
  
- });
- bot.on("message", (message) => {
-  if(message.content.startsWith("h!help")) {
-    message.channel.send(const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('a title')
-	.setURL('https://www.youtube.com/watch?v=1mOzaNFM6Jw&ab_channel=Megumianimations')
-	.setAuthor('adriana')
-	.setDescription('adriana added a description')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-channel.send(exampleEmbed););
-  }
-
 });
+
 bot.on("message", (message) => {
    if(message.content.startsWith("h!hi")) {
      message.channel.send("Hola");
