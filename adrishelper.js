ea1fe2d65fa53afada0cb5893cb08e72bce6824f
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-03 19:13:04 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -38,13 +38,26 @@ bot.on("message", function(message) {
   if(message.content.startsWith("h!invite")) {
     message.channel.send("https://discord.com/oauth2/authorize?client_id=816478380466634763&scope=bot&permissions=2146967231");
   }
-  
 });
 
-if (message.content.startsWith(prefix +"embed")){
-  message.channel.send({embed: {
-    color: 3447003,
-    description: "Esto es un simple mensaje embed."
-  }});
+if (message.content.startsWith(prefix +"embed" )){
+  const embed = new Discord.RichEmbed() 
+  .setTitle("Este es su título, puede contener 256 caracteres")
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .setColor(0x00AE86)
+  .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
+  .setFooter("Pie de página, puede contener 2048 caracteres", client.user.avatarURL)
+  .setImage(message.author.avatarURL)
+  .setThumbnail(message.author.avatarURL)
+  .setTimestamp()
+  .setURL("https://github.com/CraterMaik")
+  .addField("Este es un título de campo, puede contener 256 caracteres",
+    "Este es un valor de campo, puede contener 2048 caracteres.")
+  .addField("Campo en línea", "Debajo del campo en línea", true)
+  .addBlankField(true)
+  .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
+  
+  message.channel.send({embed});
 }
+
  bot.login(process.env.token);
