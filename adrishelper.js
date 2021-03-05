c8d1f53f95cef9d3a40bb7725881d5724c3584b5
adriana1428 <singwithmejaasecas2019@gmail.com>
2021-03-04 21:02:30 -0500

    Update adrishelper.js

--- adrishelper.js
+++ adrishelper.js
@@ -70,12 +70,12 @@ client.on("message", message => {
      if (!msg) return message.channel.send(":x: There's nothing to snipe")
 
      const SnipeEmbed = new Discord.MessageEmbed()
-     .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
-     .setDescription(msg.content)
-     .setColor("RANDOM")
-     .setImage(msg.image)
+       .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+       .setDescription(msg.content)
+       .setColor("RANDOM")
+       .setImage(msg.image)
 
-     message.channel.send(SnipeEmbed)
+       message.channel.send(SnipeEmbed)
    }
  })
  client.login(process.env.token);
